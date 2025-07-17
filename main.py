19d8e01e3a7f58865b1921735e912ae48bfc292e
devlmz <1524784033@qq.com>
2025-07-17 22:09:40 +0800

    Bug Fix

---
+++ main.py
@@ -0,0 +1,8 @@
+import interface
+import screen
+
+while True:
+    if screen.name == 'interface':
+        interface.inter()
+    else:
+        interface.inter()
--- str/main.py
+++
@@ -1,8 +0,0 @@
-import interface
-import screen
-
-while True:
-    if screen.name == 'interface':
-        interface.inter()
-    else:
-        interface.inter()
